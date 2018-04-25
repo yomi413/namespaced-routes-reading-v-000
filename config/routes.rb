df0c5759d296d74ef0ccd70a0e6b58d97b57fb29
Yomaira Escano <yjescano@gmail.com>
2018-04-25 01:39:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,5 @@ Rails.application.routes.draw do
     resources :stats, only: [:index]
   end
 
-  get '/admin/stats', to: 'stats#index'
-  get '/admin/authors/new', to: 'authors#new'
-  get '/admin/author/delete', to: 'authors#delete'
-  get '/admin/authors/create', to: 'authors#create'
-  get '/admin/comments/moderate', to: 'comments#moderate'
-
   root 'posts#index'
 end
